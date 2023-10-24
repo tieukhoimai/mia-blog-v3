057a74916e0c89ef8895cf0e3acf886a4116b4a1
Mai TIEU Khoi <57632496+tieukhoimai@users.noreply.github.com>
2023-10-24 15:39:43 +0200

    chore: add public notion link for learning section

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -3,19 +3,19 @@ const projectsData = [
     title: 'Probability for DS',
     description: `Under Construction 🚧`,
     imgSrc: '/static/learning/probability.jpeg',
-    href: '',
+    href: 'https://tieukhoimai.notion.site/Probability-c36660af652745a4bff26304a50b8a8f',
   },
   {
     title: 'Programming and Database',
     description: `Under Construction 🚧`,
     imgSrc: '/static/learning/programming.jpg',
-    href: '',
+    href: 'https://tieukhoimai.notion.site/Programming-and-Database-9739069cf4ee490cb2560807a25dca90?pvs=4',
   },
   {
     title: 'Machine Learning',
     description: `Under Construction 🚧`,
     imgSrc: '/static/learning/machine_learning.jpg',
-    href: '',
+    href: 'https://tieukhoimai.notion.site/Machine-Learning-59b0bd636e8a481b9d2f151e14aa3c13?pvs=4',
   },
 ]
 
