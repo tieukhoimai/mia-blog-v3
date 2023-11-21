bdd25f2f3425e1f7e0b73945121d5983d513144c
Mai TIEU Khoi <57632496+tieukhoimai@users.noreply.github.com>
2023-11-21 17:40:06 +0100

    feat: fix CSP blocked

--- next.config.js
+++ next.config.js
@@ -4,18 +4,15 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
 })
 
-// You might need to insert additional domains in script-src if you are using external services
 const ContentSecurityPolicy = `
   default-src 'self';
-  script-src 'self' 'unsafe-eval' 'unsafe-inline' giscus.app analytics.umami.is;
+  script-src 'self' 'unsafe-eval' 'unsafe-inline' giscus.app analytics.umami.is *.googletagmanager.com *.google-analytics.com giscus.app;
   style-src 'self' 'unsafe-inline';
   img-src * blob: data:;
   media-src *.s3.amazonaws.com;
   connect-src *;
   font-src 'self';
   frame-src giscus.app
-  script-src 'self' 'unsafe-eval' 'unsafe-inline' *.googletagmanager.com *.google-analytics.com;
-  script-src 'self' 'unsafe-eval' 'unsafe-inline' giscus.app;
 `
 
 const securityHeaders = [
