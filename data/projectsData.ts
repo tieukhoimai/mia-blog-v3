fb2d6af6719fec3410ec5c736333153c0c5634b9
Mai TIEU Khoi <57632496+tieukhoimai@users.noreply.github.com>
2024-02-21 23:13:02 +0100

    chore: update learning page

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -1,19 +1,19 @@
 const projectsData = [
   {
     title: 'Probability for DS',
-    description: `Under Construction 🚧`,
+    description: `My personal lecture notes from class Probability for data science (2023/2024) at University of Verona`,
     imgSrc: '/static/learning/probability.jpeg',
     href: 'https://tieukhoimai.notion.site/Probability-c36660af652745a4bff26304a50b8a8f',
   },
   {
     title: 'Programming and Database',
-    description: `Under Construction 🚧`,
+    description: `My personal lecture notes from class Programming and database (2023/2024) at University of Verona`,
     imgSrc: '/static/learning/programming.jpg',
     href: 'https://tieukhoimai.notion.site/Programming-and-Database-9739069cf4ee490cb2560807a25dca90?pvs=4',
   },
   {
     title: 'Machine learning',
-    description: `My personal lecture notes from my studies at University of Verona (2023/2024) to reflects my understanding and interpretation of the course material`,
+    description: `My personal lecture notes from class Machine learning for data science (2023/2024) at University of Verona`,
     imgSrc: '/static/learning/machine_learning.jpg',
     href: 'https://tieukhoimai.notion.site/Machine-Learning-59b0bd636e8a481b9d2f151e14aa3c13?pvs=4',
   },
