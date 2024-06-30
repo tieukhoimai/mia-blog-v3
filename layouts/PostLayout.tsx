6accbbb3240939df0404dae100fffaa33f33a2d8
Mai TIEU Khoi <57632496+tieukhoimai@users.noreply.github.com>
2024-07-01 00:02:48 +0200

    chore: remove comment

--- layouts/PostLayout.tsx
+++ layouts/PostLayout.tsx
@@ -106,10 +106,6 @@ export default function PostLayout({ content, authorDetails, next, prev, childre
               </div>
               <div className="prose max-w-none pb-8 pt-10 dark:prose-invert">{children}</div>
               <div className=" pb-6 pt-6 text-sm text-gray-700 dark:text-gray-300">
-                {/* <Link href={discussUrl(path)} rel="nofollow">
-                  Discuss on Twitter
-                </Link>
-                {` • `} */}
                 <Link href={editUrl(filePath)}>View on GitHub</Link>
               </div>
               {siteMetadata.comments && (
