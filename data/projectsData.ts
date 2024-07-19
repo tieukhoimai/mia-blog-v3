2b2dd8e3856360735e996d83929933d8bc559b26
Mai TIEU Khoi <57632496+tieukhoimai@users.noreply.github.com>
2024-07-19 23:53:15 +0200

    feat: add one course

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -7,22 +7,28 @@ interface Project {
 
 const projectsData: Project[] = [
   {
-    title: 'Probability for DS',
-    description: `My personal lecture notes from class Probability for data science (2023/2024) at University of Verona`,
+    title: 'Probability for Data Science',
+    description: `My personal lecture notes from course Probability for data science (2023/2024) at University of Verona`,
     imgSrc: '/static/learning/probability.jpeg',
     href: 'https://tieukhoimai.notion.site/Probability-c36660af652745a4bff26304a50b8a8f',
   },
   {
     title: 'Programming and Database',
-    description: `My personal lecture notes from class Programming and database (2023/2024) at University of Verona`,
+    description: `My personal lecture notes from course Programming and database (2023/2024) at University of Verona`,
     imgSrc: '/static/learning/programming.jpg',
     href: 'https://tieukhoimai.notion.site/Programming-and-Database-9739069cf4ee490cb2560807a25dca90?pvs=4',
   },
   {
     title: 'Machine learning',
-    description: `My personal lecture notes from class Machine learning for data science (2023/2024) at University of Verona`,
+    description: `My personal lecture notes from course Machine learning for data science (2023/2024) at University of Verona`,
     imgSrc: '/static/learning/machine_learning.jpg',
     href: 'https://tieukhoimai.notion.site/Machine-Learning-59b0bd636e8a481b9d2f151e14aa3c13?pvs=4',
+  },
+  {
+    title: 'Statistical learning',
+    description: `My personal lecture notes from course Statistical learning (2023/2024) at University of Verona`,
+    imgSrc: '/static/learning/statistical_learning.jpg',
+    href: 'https://tieukhoimai.notion.site/Statistical-Learning-927bd5d75bce4999b8f78db0944e1b1d?pvs=4',
   },
 ]
 
